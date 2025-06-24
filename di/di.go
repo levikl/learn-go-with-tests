cd82c369d79da11ef6b288c9cbee542fdd546a3c
Levi Klingler <levikl@geeked.gg>
2025-06-24 04:50:27 -0400

    feat: dependency injection/write to `http.ResponseWriter`

--- di/di.go
+++ di/di.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
-	fmt.Fprintf(writer, "Hello, %s", name)
+	_, err := fmt.Fprintf(writer, "Hello, %s", name)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
 }
 
 func main() {
-	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
