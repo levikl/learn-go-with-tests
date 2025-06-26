62b92dc524798083f4960a8ab1b6163b094df085
Levi Klingler <levikl@geeked.gg>
2025-06-26 04:55:43 -0400

    feat: library consumer-driven TDD + `io/fs`

---
+++ reading-files/post.go
@@ -0,0 +1,50 @@
+package blogposts
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+type Post struct {
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
+
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
+	}
+
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // consume '---' line
+
+	var result strings.Builder
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		result.WriteString(text)
+		result.WriteString("\n")
+	}
+
+	return strings.TrimSuffix(result.String(), "\n")
+}
