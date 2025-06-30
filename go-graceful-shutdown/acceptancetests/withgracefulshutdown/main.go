bbce15e52e90e1f627415e270528247c746ff339
Levi Klingler <levikl@geeked.gg>
2025-06-30 17:48:41 -0400

    feat: hand-rolled acceptance (functional/e2e) tests

---
+++ go-graceful-shutdown/acceptancetests/withgracefulshutdown/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	gracefulshutdown "github.com/levikl/learn-go-with-tests/go-graceful-shutdown"
+	"github.com/levikl/learn-go-with-tests/go-graceful-shutdown/acceptancetests"
+)
+
+func main() {
+	var (
+		ctx        = context.Background()
+		httpServer = &http.Server{
+			Addr:    ":8080",
+			Handler: http.HandlerFunc(acceptancetests.SlowHandler),
+		}
+		server = gracefulshutdown.NewServer(httpServer)
+	)
+
+	if err := server.ListenAndServe(ctx); err != nil {
+		// this will typically happen if our responses aren't written before the ctx deadline, not much can be done
+		log.Fatalf("uh oh, didnt shutdown gracefully, some responses may have been lost %v", err)
+	}
+
+	// hopefully, you'll always see this instead
+	log.Println("shutdown gracefully! all responses were sent")
+}
