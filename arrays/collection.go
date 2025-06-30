62f8bd097fed4d7fe872636c722b70acafc100d0
Levi Klingler <levikl@geeked.gg>
2025-06-30 16:45:44 -0400

    feat: generics pt 4 - implement `Find`

--- arrays/collection.go
+++ arrays/collection.go
@@ -7,3 +7,12 @@ func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
 	}
 	return result
 }
+
+func Find[A any](items []A, predicate func(A) bool) (item A, found bool) {
+	for _, x := range items {
+		if predicate(x) {
+			return x, true
+		}
+	}
+	return
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -56,3 +56,34 @@ func TestReduce(t *testing.T) {
 		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
 	})
 }
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+		AssertTrue(t, found)
+		AssertEqual(t, firstEvenNumber, 2)
+	})
+
+	type Person struct {
+		Name string
+	}
+
+	t.Run("find the best programmer", func(t *testing.T) {
+		people := []Person{
+			{Name: "Kent Beck"},
+			{Name: "Leroy Jenkins"},
+			{Name: "Martin Fowler"},
+		}
+
+		king, found := Find(people, func(x Person) bool {
+			return x.Name == "Leroy Jenkins"
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, king, Person{Name: "Leroy Jenkins"})
+	})
+}
