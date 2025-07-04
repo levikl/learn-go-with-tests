36c6f300ad545fd74d76d8560a7672c3b5208aad
Levi Klingler <levikl@geeked.gg>
2025-07-04 18:21:11 -0400

    feat: add POST handler to record wins

---
+++ http-server/server_integration_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+)
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := NewInMemoryPlayerStore()
+	server := PlayerServer{store}
+	player := "Pepper"
+	winCount := 3
+
+	for range winCount {
+		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	}
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+
+	assertStatus(t, response.Code, http.StatusOK)
+	assertResponseBody(t, response.Body.String(), strconv.Itoa(winCount))
+}
