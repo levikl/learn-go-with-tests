06133a4a12361fc431929064c66cc6ed93ca65b8
Levi Klingler <levikl@geeked.gg>
2025-07-15 01:28:00 -0400

    feat: stub out new poker blinds time driven alerts

--- http-server/cli_test.go
+++ http-server/cli_test.go
@@ -7,12 +7,14 @@ import (
 	poker "github.com/levikl/learn-go-with-tests/http-server"
 )
 
+var dummySpyAlerter = &poker.SpyBlindAlerter{}
+
 func TestCLI(t *testing.T) {
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
 		playerStore := &poker.StubPlayerStore{}
 
-		cli := poker.NewCLI(playerStore, in)
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Chris")
@@ -22,9 +24,22 @@ func TestCLI(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
 		playerStore := &poker.StubPlayerStore{}
 
-		cli := poker.NewCLI(playerStore, in)
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Cleo")
+	})
+
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &poker.SpyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(blindAlerter.Alerts) != 1 {
+			t.Fatal("expected a blind alert to be scheduled")
+		}
 	})
 }
--- http-server/testing.go
+++ http-server/testing.go
@@ -4,6 +4,7 @@ import (
 	"net/http/httptest"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type StubPlayerStore struct {
@@ -77,4 +78,18 @@ func AssertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
 	if store.winCalls[0] != winner {
 		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
 	}
+}
+
+type SpyBlindAlerter struct {
+	Alerts []struct {
+		scheduledAt time.Duration
+		amount      int
+	}
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.Alerts = append(s.Alerts, struct {
+		scheduledAt time.Duration
+		amount      int
+	}{duration, amount})
 }
