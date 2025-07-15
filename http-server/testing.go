246e52977abe3b4c0236863d775f7effcb7cec7b
Levi Klingler <levikl@geeked.gg>
2025-07-15 01:41:26 -0400

    feat: iterate over array of blinds and call scheduler

--- http-server/testing.go
+++ http-server/testing.go
@@ -82,14 +82,14 @@ func AssertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
 
 type SpyBlindAlerter struct {
 	Alerts []struct {
-		scheduledAt time.Duration
-		amount      int
+		ScheduledAt time.Duration
+		Amount      int
 	}
 }
 
 func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
 	s.Alerts = append(s.Alerts, struct {
-		scheduledAt time.Duration
-		amount      int
+		ScheduledAt time.Duration
+		Amount      int
 	}{duration, amount})
 }
